3a1e4f05aadd3313430e4b4e49aa5426516e8e99
joeyaranda <32945946+joeyaranda@users.noreply.github.com>
2018-01-26 07:51:20 +0800

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,13 +1,16 @@
+#Set working directory
 setwd("C:\\")
+
+#Install and load the needed package
 install.packages("plyr")
 library(plyr)
 
-#download file from the given URL
+#Download file from the given URL
 if(!file.exists("./data")){dir.create("./data")}
 fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 download.file(fileUrl,destfile="./data/Dataset.zip")
 
-# Unzip dataSet to /data directory
+#Unzip dataSet to /data directory
 unzip(zipfile="./data/Dataset.zip",exdir="./data")
 
 #Load activity and features datasets
